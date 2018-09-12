5e354d035e6c52686bdfbd611a0adda381d55b8f
Modestas <modestask97@gmail.com>
2018-09-12 17:49:45 +0100

    meme reducer fucntion

---
+++ src/reducers/index.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux';
+import { RECIEVE_MEMES } from '../actions';
+
+function memes(state = [], action) {
+  switch(action.type) {
+    case RECIEVE_MEMES:
+      return action.memes;
+    default:
+      return state;
+  }
+}
